7572478fb6445ad05542f173ad3524f650a91021
Leonel César <leonelcesar62rgmail.com>
2025-11-07 13:46:00 +0100

    Feat: paragrafo adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ function Home() {
     <main className="">
       <h1>Hello Next JS and World!</h1>
       <strong className="text-muted">Meu nome é Leonel</strong>
+      <p>Estou criando um dashboard do zero</p>
     </main>
   );
 }
